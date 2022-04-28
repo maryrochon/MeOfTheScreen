53c2f678ba585dd81b47dcf18e3a5523553123db
maryrochon <99832210+maryrochon@users.noreply.github.com>
2022-04-28 18:33:36 -0400

    Add files via upload

---
+++ javascript/stn.js
@@ -0,0 +1,211 @@
+//snowflakes
+$(document).scroll(function(){
+   $("#graphtitle").addClass("move1")
+   $("#graphtitle:nth-child(2)").addClass("move2")
+   $("#graphtitle:nth-child(3)").addClass("move3")
+   $("#horlabel").addClass("move4")
+   $("#horlabel:nth-child(2)").addClass("move5")
+   $("#horlabel:nth-child(3)").addClass("move6")
+   $("#vertlabel").addClass("move7")
+   $("#vertlabel:nth-child(2)").addClass("move8")
+   $("#vertlabel:nth-child(3)").addClass("move9")
+   $("#verticalgraph").addClass("move10")
+   $("#verticalgraph:nth-child(2)").addClass("move11")
+   $("#verticalgraph:nth-child(3)").addClass("move12")
+   $("#horizontalgraph").addClass("move13")
+   $("#horizontalgraph:nth-child(2)").addClass("move14")
+   $("#horizontalgraph:nth-child(3)").addClass("move15")
+   $("#horvalue1").addClass("move16")
+   $("#hornum1").addClass("move16")
+   $("#horvalue2").addClass("move16")
+   $("#hornum2").addClass("move16")
+   $("#horvalue3").addClass("move16")
+   $("#hornum3").addClass("move16")
+   $("#horvalue4").addClass("move16")
+   $("#hornum4").addClass("move16")
+   $("#horvalue5").addClass("move16")
+   $("#hornum5").addClass("move16")
+   $("#horvalue6").addClass("move16")
+   $("#hornum6").addClass("move16")
+   $("#horvalue7").addClass("move16")
+   $("#hornum7").addClass("move16")
+   $("#horvalue8").addClass("move16")
+   $("#hornum8").addClass("move16")
+   $("#horvalue9").addClass("move16")
+   $("#hornum9").addClass("move16")
+   $("#horvalue10").addClass("move16")
+   $("#hornum10").addClass("move16")
+   $("#vertvalue1").addClass("move16")
+   $("#vertnum1").addClass("move16")
+   $("#vertvalue2").addClass("move16")
+   $("#vertnum2").addClass("move16")
+   $("#vertvalue3").addClass("move16")
+   $("#vertnum3").addClass("move16")
+   $("#vertvalue4").addClass("move16")
+   $("#vertnum4").addClass("move16")
+   $("#vertvalue5").addClass("move16")
+   $("#vertnum5").addClass("move16")
+   $("#vertvalue6").addClass("move16")
+   $("#vertnum6").addClass("move16")
+   $("#vertvalue7").addClass("move16")
+   $("#vertnum7").addClass("move16")
+   $("#vertvalue8").addClass("move16")
+   $("#vertnum8").addClass("move16")
+   $("#vertvalue9").addClass("move16")
+   $("#vertnum9").addClass("move16")
+   $("#vertvalue10").addClass("move16")
+   $("#vertnum10").addClass("move16")
+   $("#stn1").addClass("move17")
+   $("#stn2").addClass("move17")
+   $("#stn3").addClass("move17")
+   $("#stn4").addClass("move17")
+   $("#stn5").addClass("move17")
+   $("#stn6").addClass("move17")
+   $("#stn7").addClass("move17")
+   $("#stn8").addClass("move17")
+   $("#stn9").addClass("move17")
+   $("#stn10").addClass("move17")
+   $("#stn11").addClass("move17")
+   $("#stn12").addClass("move17")
+   $("#stn13").addClass("move17")
+   $("#stn14").addClass("move17")
+   $("#stn15").addClass("move17")
+
+})
+
+//graph
+$("#stn1").hover(function(){
+    $(".graph").toggleClass("stn1")
+    if($("#stn1:hover").length == 1) {
+    $("#sky1").css("opacity", "1")
+    }
+ else{
+    $("#sky1").css("opacity", "0")
+ }
+})
+$("#stn2").hover(function(){
+    $(".graph").toggleClass("stn2")
+    if($("#stn2:hover").length == 1) {
+    $("#sky2").css("opacity", "1")
+    }
+ else{
+    $("#sky2").css("opacity", "0")
+ }
+})
+$("#stn3").hover(function(){
+    $(".graph").toggleClass("stn3")
+    if($("#stn3:hover").length == 1) {
+    $("#sky3").css("opacity", "1")
+    }
+ else{
+    $("#sky3").css("opacity", "0")
+ }
+})
+$("#stn4").hover(function(){
+    $(".graph").toggleClass("stn4")
+    if($("#stn4:hover").length == 1) {
+    $("#sky4").css("opacity", "1")
+    }
+ else{
+    $("#sky4").css("opacity", "0")
+ }
+})
+$("#stn5").hover(function(){
+    $(".graph").toggleClass("stn5")
+    if($("#stn5:hover").length == 1) {
+    $("#sky5").css("opacity", "1")
+    }
+ else{
+    $("#sky5").css("opacity", "0")
+ }
+})
+$("#stn6").hover(function(){
+    $(".graph").toggleClass("stn6")
+    if($("#stn6:hover").length == 1) {
+    $("#sky6").css("opacity", "1")
+    }
+ else{
+    $("#sky6").css("opacity", "0")
+ }
+})
+$("#stn7").hover(function(){
+    $(".graph").toggleClass("stn7")
+    if($("#stn7:hover").length == 1) {
+    $("#sky7").css("opacity", "1")
+    }
+ else{
+    $("#sky7").css("opacity", "0")
+ }
+})
+$("#stn8").hover(function(){
+    $(".graph").toggleClass("stn8")
+    if($("#stn8:hover").length == 1) {
+    $("#sky8").css("opacity", "1")
+    }
+ else{
+    $("#sky8").css("opacity", "0")
+ }
+})
+$("#stn9").hover(function(){
+    $(".graph").toggleClass("stn9")
+    if($("#stn9:hover").length == 1) {
+    $("#sky9").css("opacity", "1")
+    }
+ else{
+    $("#sky9").css("opacity", "0")
+ }
+})
+$("#stn10").hover(function(){
+    $(".graph").toggleClass("stn10")
+    if($("#stn10:hover").length == 1) {
+    $("#sky10").css("opacity", "1")
+    }
+ else{
+    $("#sky10").css("opacity", "0")
+ }
+})
+$("#stn11").hover(function(){
+    $(".graph").toggleClass("stn11")
+    if($("#stn11:hover").length == 1) {
+    $("#sky11").css("opacity", "1")
+    }
+ else{
+    $("#sky11").css("opacity", "0")
+ }
+})
+$("#stn12").hover(function(){
+    $(".graph").toggleClass("stn12")
+    if($("#stn12:hover").length == 1) {
+    $("#sky12").css("opacity", "1")
+    }
+ else{
+    $("#sky12").css("opacity", "0")
+ }
+})
+$("#stn13").hover(function(){
+    $(".graph").toggleClass("stn13")
+    if($("#stn13:hover").length == 1) {
+    $("#sky13").css("opacity", "1")
+    }
+ else{
+    $("#sky13").css("opacity", "0")
+ }
+})
+$("#stn14").hover(function(){
+    $(".graph").toggleClass("stn14")
+    if($("#stn14:hover").length == 1) {
+    $("#sky14").css("opacity", "1")
+    }
+ else{
+    $("#sky14").css("opacity", "0")
+ }
+})
+$("#stn15").hover(function(){
+    $(".graph").toggleClass("stn15")
+    if($("#stn15:hover").length == 1) {
+    $("#sky15").css("opacity", "1")
+    }
+ else{
+    $("#sky15").css("opacity", "0")
+ }
+})
